57eeef62ecdd62f14a773beafa2a027025857280
TakumiSenaha <oretatchun.no.10@gmail.com>
2023-05-13 11:30:29 +0900

    second commit

---
+++ main.py
@@ -0,0 +1,43 @@
+from tuning import Tuning
+import flyobj
+import touchsensor
+import vol_and_text as voice
+import DOA
+import numpy as np
+import usb.core
+import usb.util
+import pygame
+import time
+import RPi.GPIO as GPIO
+
+## 全画面黒
+def main_loop():
+    v = voice.Voice(device_index=0)
+    flyobj.init()
+    while True:
+        touchsensor.initial_process()
+        while True:
+            touch = touchsensor.read_touchsensor()
+            if(touch == 1):
+                break
+        dev = usb.core.find(idVendor=0x2886, idProduct=0x0018)
+        if dev:
+            Mic_tuning = Tuning(dev)
+            while(flyobj.display_thread.is_alive()):
+                dB = v.get_dB()
+                if(dB >= -50):
+                    flyobj.gen_triangle(angle=180 - Mic_tuning.direction, scale=(dB+50)/20)
+                touch = touchsensor.read_touchsensor()
+                if(touch == 1):
+                    break
+                time.sleep(0.1)
+
+if __name__ == '__main__':
+    try:
+        main_loop()
+        pass
+    except KeyboardInterrupt:
+        pass
+    pass
+GPIO.cleanup()
+
